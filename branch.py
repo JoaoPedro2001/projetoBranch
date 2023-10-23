3677ddf6aaafd50f33e0397ffe955b738784a867
JoaoPedro2001 <147657992+JoaoPedro2001@users.noreply.github.com>
2023-10-23 15:50:40 -0300

    Create branch.py

---
+++ branch.py
@@ -0,0 +1,9 @@
+lista = []
+for recebe in range(2):
+    nome = input('Nome: ')
+    idade = input('Idade: ')
+    profissao = input('Profissão: ')
+    lista.append(nome)
+    lista.append(idade)
+    lista.append(profissao)
+print(lista)
